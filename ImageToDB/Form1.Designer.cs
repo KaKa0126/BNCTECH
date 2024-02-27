532a21481c1b4b6159c22b0c7a57432198711b5b
KaKa0126 <okhhs0126@hanmail.net>
2024-02-27 16:51:29 +0900

    240227

---
+++ ImageToDB/Form1.Designer.cs
@@ -0,0 +1,258 @@
+﻿namespace TEST2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Pic1 = new System.Windows.Forms.PictureBox();
+            this.Pic2 = new System.Windows.Forms.PictureBox();
+            this.BtnStart = new System.Windows.Forms.Button();
+            this.BtnCapture = new System.Windows.Forms.Button();
+            this.BtnDBinsert = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.Pic3 = new System.Windows.Forms.PictureBox();
+            this.BtnSerachingDB = new System.Windows.Forms.Button();
+            this.dateTimePicker = new System.Windows.Forms.DateTimePicker();
+            this.comboBoxTitles = new System.Windows.Forms.ComboBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.Pic4 = new System.Windows.Forms.PictureBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.TXT_Result = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.Pic1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Pic2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Pic3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Pic4)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // Pic1
+            // 
+            this.Pic1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.Pic1.Location = new System.Drawing.Point(39, 94);
+            this.Pic1.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
+            this.Pic1.Name = "Pic1";
+            this.Pic1.Size = new System.Drawing.Size(431, 358);
+            this.Pic1.TabIndex = 0;
+            this.Pic1.TabStop = false;
+            // 
+            // Pic2
+            // 
+            this.Pic2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.Pic2.Location = new System.Drawing.Point(537, 94);
+            this.Pic2.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
+            this.Pic2.Name = "Pic2";
+            this.Pic2.Size = new System.Drawing.Size(431, 358);
+            this.Pic2.TabIndex = 1;
+            this.Pic2.TabStop = false;
+            // 
+            // BtnStart
+            // 
+            this.BtnStart.Location = new System.Drawing.Point(39, 560);
+            this.BtnStart.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
+            this.BtnStart.Name = "BtnStart";
+            this.BtnStart.Size = new System.Drawing.Size(173, 130);
+            this.BtnStart.TabIndex = 2;
+            this.BtnStart.Text = "Start";
+            this.BtnStart.UseVisualStyleBackColor = true;
+            this.BtnStart.Click += new System.EventHandler(this.BtnStart_Click);
+            // 
+            // BtnCapture
+            // 
+            this.BtnCapture.Location = new System.Drawing.Point(537, 560);
+            this.BtnCapture.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
+            this.BtnCapture.Name = "BtnCapture";
+            this.BtnCapture.Size = new System.Drawing.Size(173, 130);
+            this.BtnCapture.TabIndex = 3;
+            this.BtnCapture.Text = "Capture";
+            this.BtnCapture.UseVisualStyleBackColor = true;
+            this.BtnCapture.Click += new System.EventHandler(this.BtnCapture_Click);
+            // 
+            // BtnDBinsert
+            // 
+            this.BtnDBinsert.Location = new System.Drawing.Point(1027, 560);
+            this.BtnDBinsert.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
+            this.BtnDBinsert.Name = "BtnDBinsert";
+            this.BtnDBinsert.Size = new System.Drawing.Size(173, 130);
+            this.BtnDBinsert.TabIndex = 4;
+            this.BtnDBinsert.Text = "DB Insert";
+            this.BtnDBinsert.UseVisualStyleBackColor = true;
+            this.BtnDBinsert.Click += new System.EventHandler(this.BtnDBinsert_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.BackColor = System.Drawing.SystemColors.Menu;
+            this.textBox1.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBox1.Location = new System.Drawing.Point(39, 40);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(152, 28);
+            this.textBox1.TabIndex = 5;
+            this.textBox1.Text = "Camera Live";
+            // 
+            // textBox2
+            // 
+            this.textBox2.BackColor = System.Drawing.SystemColors.Menu;
+            this.textBox2.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBox2.Location = new System.Drawing.Point(537, 40);
+            this.textBox2.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(173, 28);
+            this.textBox2.TabIndex = 6;
+            this.textBox2.Text = "Capture image";
+            // 
+            // Pic3
+            // 
+            this.Pic3.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.Pic3.Location = new System.Drawing.Point(537, 802);
+            this.Pic3.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
+            this.Pic3.Name = "Pic3";
+            this.Pic3.Size = new System.Drawing.Size(431, 358);
+            this.Pic3.TabIndex = 7;
+            this.Pic3.TabStop = false;
+            // 
+            // BtnSerachingDB
+            // 
+            this.BtnSerachingDB.Location = new System.Drawing.Point(39, 878);
+            this.BtnSerachingDB.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
+            this.BtnSerachingDB.Name = "BtnSerachingDB";
+            this.BtnSerachingDB.Size = new System.Drawing.Size(173, 130);
+            this.BtnSerachingDB.TabIndex = 8;
+            this.BtnSerachingDB.Text = "DB Searching";
+            this.BtnSerachingDB.UseVisualStyleBackColor = true;
+            this.BtnSerachingDB.Click += new System.EventHandler(this.BtnSerachingDB_Click);
+            // 
+            // dateTimePicker
+            // 
+            this.dateTimePicker.Location = new System.Drawing.Point(39, 802);
+            this.dateTimePicker.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
+            this.dateTimePicker.Name = "dateTimePicker";
+            this.dateTimePicker.Size = new System.Drawing.Size(368, 35);
+            this.dateTimePicker.TabIndex = 9;
+            // 
+            // comboBoxTitles
+            // 
+            this.comboBoxTitles.FormattingEnabled = true;
+            this.comboBoxTitles.Location = new System.Drawing.Point(39, 1020);
+            this.comboBoxTitles.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
+            this.comboBoxTitles.Name = "comboBoxTitles";
+            this.comboBoxTitles.Size = new System.Drawing.Size(368, 32);
+            this.comboBoxTitles.TabIndex = 10;
+            // 
+            // textBox3
+            // 
+            this.textBox3.BackColor = System.Drawing.SystemColors.Menu;
+            this.textBox3.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBox3.Location = new System.Drawing.Point(1027, 40);
+            this.textBox3.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(173, 28);
+            this.textBox3.TabIndex = 11;
+            this.textBox3.Text = "AI Output image";
+            // 
+            // Pic4
+            // 
+            this.Pic4.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.Pic4.Location = new System.Drawing.Point(1027, 94);
+            this.Pic4.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
+            this.Pic4.Name = "Pic4";
+            this.Pic4.Size = new System.Drawing.Size(431, 358);
+            this.Pic4.TabIndex = 12;
+            this.Pic4.TabStop = false;
+            // 
+            // textBox4
+            // 
+            this.textBox4.BackColor = System.Drawing.SystemColors.Menu;
+            this.textBox4.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBox4.Location = new System.Drawing.Point(1027, 494);
+            this.textBox4.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(173, 28);
+            this.textBox4.TabIndex = 13;
+            this.textBox4.Text = "Result";
+            // 
+            // TXT_Result
+            // 
+            this.TXT_Result.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.TXT_Result.Location = new System.Drawing.Point(1116, 494);
+            this.TXT_Result.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
+            this.TXT_Result.Name = "TXT_Result";
+            this.TXT_Result.Size = new System.Drawing.Size(344, 28);
+            this.TXT_Result.TabIndex = 14;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(13F, 24F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1642, 1295);
+            this.Controls.Add(this.TXT_Result);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.Pic4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.comboBoxTitles);
+            this.Controls.Add(this.dateTimePicker);
+            this.Controls.Add(this.BtnSerachingDB);
+            this.Controls.Add(this.Pic3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.BtnDBinsert);
+            this.Controls.Add(this.BtnCapture);
+            this.Controls.Add(this.BtnStart);
+            this.Controls.Add(this.Pic2);
+            this.Controls.Add(this.Pic1);
+            this.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            ((System.ComponentModel.ISupportInitialize)(this.Pic1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Pic2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Pic3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Pic4)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox Pic1;
+        private System.Windows.Forms.PictureBox Pic2;
+        private System.Windows.Forms.Button BtnStart;
+        private System.Windows.Forms.Button BtnCapture;
+        private System.Windows.Forms.Button BtnDBinsert;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.PictureBox Pic3;
+        private System.Windows.Forms.Button BtnSerachingDB;
+        private System.Windows.Forms.DateTimePicker dateTimePicker;
+        private System.Windows.Forms.ComboBox comboBoxTitles;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.PictureBox Pic4;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.TextBox TXT_Result;
+    }
+}
+
---
+++ ImageToDB/Form1.cs
@@ -0,0 +1,348 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using AForge.Video;
+using AForge.Video.DirectShow;
+using Npgsql;
+using NpgsqlTypes;
+
+namespace TEST2
+{
+    public partial class Form1 : Form
+    {
+        private string lastFileName = null; // 최근에 확인한 파일명 저장 변수
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        VideoCaptureDevice videoCapture;
+        FilterInfoCollection filterInfo;
+
+        void StartCamera()
+        {
+            try
+            {
+                filterInfo = new FilterInfoCollection(FilterCategory.VideoInputDevice);
+                videoCapture = new VideoCaptureDevice(filterInfo[0].MonikerString);
+                videoCapture.NewFrame += new NewFrameEventHandler(Camera_On);
+                videoCapture.Start();
+
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+
+        }
+
+        private void Camera_On(object sender, NewFrameEventArgs eventArgs)
+        {
+            Pic1.Image = (Bitmap)eventArgs.Frame.Clone();
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            try
+            {
+                videoCapture.Stop();
+            }
+            catch
+            {
+                return;
+            }
+        }
+
+        private void BtnStart_Click(object sender, EventArgs e)
+        {
+            StartCamera();
+        }
+
+        private void BtnCapture_Click(object sender, EventArgs e)
+        {
+            // Copy image from Pic1 to Pic2
+            Pic2.Image = (Image)Pic1.Image.Clone();
+
+            string saveName = DateTime.Now.ToString("yyMMddhhmmss_1");
+            string year = DateTime.Now.ToString("yyyy");
+            string month = DateTime.Now.ToString("MM");
+            string day = DateTime.Now.ToString("dd");
+            string folderPath1 = @"C:\Users\PJK_BNC-TECH\Desktop\folder1";
+            string folderPath2 = @"D:\Robot\ai_output\" + year + @"\" + month + @"\" + day + @"\89500-DO221DFS";
+            string folderPath3 = @"D:\Robot\ai_result\" + year + @"\" + month + @"\" + day + @"\89500-DO221DFS";
+
+            // Ensure the folders exist before trying to save
+            Directory.CreateDirectory(folderPath1);
+            Directory.CreateDirectory(folderPath2);
+
+            string fileName1 = Path.Combine(folderPath1, saveName + ".jpg");
+            string fileName2 = Path.Combine(folderPath2, saveName + ".jpg");
+
+            try
+            {
+                var bitmap = new Bitmap(Pic2.Width, Pic2.Height);
+                Pic2.DrawToBitmap(bitmap, Pic2.ClientRectangle);
+
+                // Save image to the first location
+                bitmap.Save(fileName1, System.Drawing.Imaging.ImageFormat.Jpeg);
+
+                // Copy the saved image to the second location
+                File.Copy(fileName1, fileName2, true);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.ToString(), "Error");
+            }
+
+            // Start monitoring the second folder
+            StartFolderMonitoring(folderPath3);
+            /*
+            finally
+            {
+                // Dispose of the Bitmap object
+                bitmap.Dispose();
+            }
+            */
+        }
+
+        private void StartFolderMonitoring(string folderPath)
+        {
+            // Create a new FileSystemWatcher
+            FileSystemWatcher watcher = new FileSystemWatcher();
+            watcher.Path = folderPath;
+
+            // Subscribe to the Created event
+            watcher.Created += (sender, e) =>
+            {
+                // Wait for a short time to ensure that the file creation is complete
+                System.Threading.Thread.Sleep(100);
+
+                // Check if the file still exists (it may have been deleted)
+                if (File.Exists(e.FullPath))
+                {
+                    // Handle the newly created file
+                    string newFileName = Path.GetFileName(e.FullPath);
+                    string fileNamePart = newFileName.Substring(15, 5); // 일부 파일명 추출
+
+                    // Update the lastFileName variable
+                    lastFileName = newFileName;
+
+                    // Use Invoke to update UI controls
+                    Invoke((MethodInvoker)delegate
+                    {
+                        // Display fileNamePart in TextBox
+                        TXT_Result.Text = fileNamePart;
+
+                        // Load the image from the file path
+                        Image originalImage = Image.FromFile(e.FullPath);
+
+                        // Calculate the aspect ratio of the original image
+                        float aspectRatio = (float)originalImage.Width / (float)originalImage.Height;
+
+                        // Create a new Bitmap with the desired size (e.g., the size of the PictureBox)
+                        int targetWidth = Pic4.Width;
+                        int targetHeight = (int)(targetWidth / aspectRatio);
+                        Bitmap resizedImage = new Bitmap(originalImage, new Size(targetWidth, targetHeight));
+
+                        // Set the resized image to the PictureBox
+                        Pic4.Image = resizedImage;
+                    });
+
+                    // Perform any additional actions with the new file
+                    // Console.WriteLine($"New File Detected: {newFileName}, Partial Name: {fileNamePart}");
+                }
+            };
+
+            // Enable the FileSystemWatcher
+            watcher.EnableRaisingEvents = true;
+        }
+
+        private void BtnDBinsert_Click(object sender, EventArgs e)
+        {
+            string connString = "Host=localhost;Port=5432;Username=test;Password=test;Database=TEST2";
+
+            string year = DateTime.Now.ToString("yyyy");
+            string month = DateTime.Now.ToString("MM");
+            string day = DateTime.Now.ToString("dd");
+            string filePath = @"D:\Robot\ai_result\" + year + @"\" + month + @"\" + day + @"\89500-DO221DFS";
+
+            //finding recentFileName 
+            DirectoryInfo DI = new DirectoryInfo(filePath);
+
+            // 디렉토리에서 파일 목록을 가져오고 날짜/시간 기준으로 정렬
+            FileInfo[] files = DI.GetFiles().OrderByDescending(f => f.CreationTime).ToArray();
+
+            // 'recentFileName' 변수에 가장 최근에 생성된 파일명 설정
+            string recentFileName = files.Length > 0 ? files[0].Name : null;
+
+            //finding result
+            string fullPath = filePath + @"\" + recentFileName + ".jpg";
+            string fileNameWithExtension = Path.GetFileName(fullPath);
+            string resultPart = fileNameWithExtension.Substring(15, 5);  //15번째 부터, 5글자 추출
+
+            // Declare MemoryStream, Image, and byte array
+            using (MemoryStream ms = new MemoryStream())
+            using (Image img = Pic2.Image)
+            {
+                try
+                {
+                    img.Save(ms, System.Drawing.Imaging.ImageFormat.Jpeg);
+                    byte[] bytes = ms.ToArray();
+
+                    // PostgreSQL db open
+                    using (var conn = new NpgsqlConnection(connString))
+                    {
+                        conn.Open();
+
+                        // DB INSERT statement
+                        using (var command = new NpgsqlCommand())
+                        {
+                            command.Connection = conn;
+                            command.CommandText = "INSERT INTO data VALUES(@created, @title, @result, @photo)";
+
+                            // Adjust NpgsqlDbType according to the actual data types in your database
+                            command.Parameters.AddWithValue("created", NpgsqlDbType.Timestamp, DateTime.Now);
+                            command.Parameters.AddWithValue("title", NpgsqlDbType.Text, recentFileName);
+                            command.Parameters.AddWithValue("result", NpgsqlDbType.Text, resultPart);
+                            command.Parameters.AddWithValue("photo", NpgsqlDbType.Bytea, bytes);
+
+                            command.ExecuteNonQuery();
+                        }
+                    }
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.ToString());
+                }
+            }
+            // No need to explicitly close the connection; 'using' statement handles it
+        }
+
+        //디자인탭에서 선택한 날짜의 'title' 열의 결과값 조회
+        private void BtnSerachingDB_Click(object sender, EventArgs e)
+        {
+            string connString = "Host=localhost;Port=5432;Username=test;Password=test;Database=TEST2";
+
+            // 선택된 날짜 가져오기
+            DateTime targetDate = dateTimePicker.Value.Date;
+
+            using (var conn = new NpgsqlConnection(connString))
+            {
+                try
+                {
+                    conn.Open();
+
+                    // 특정 날짜의 'title' 조회 쿼리
+                    string selectQuery = "SELECT title FROM data WHERE created::date = @targetDate::date";
+
+                    using (var selectCommand = new NpgsqlCommand(selectQuery, conn))
+                    {
+                        // 파라미터 추가
+                        selectCommand.Parameters.AddWithValue("targetDate", targetDate);
+
+                        // 'title' 값을 저장할 목록
+                        List<string> titleList = new List<string>();
+
+                        using (var reader = selectCommand.ExecuteReader())
+                        {
+                            while (reader.Read())
+                            {
+                                // 'title' 값을 읽어와 목록에 추가
+                                string title = reader.GetString(reader.GetOrdinal("title"));
+                                titleList.Add(title);
+                            }
+                        }
+
+                        // 'titleList'를 사용하여 ComboBox에 항목 추가
+                        comboBoxTitles.Items.Clear();
+                        comboBoxTitles.Items.AddRange(titleList.ToArray());
+
+                        // 선택한 결과를 처리하는 이벤트 핸들러 등록
+                        comboBoxTitles.SelectedIndexChanged += ComboBoxTitles_SelectedIndexChanged;
+                    }
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.ToString());
+                }
+            }
+        }
+
+        //이미지를 디자인 탭에서 표시
+        private void ComboBoxTitles_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            // ComboBox에서 선택된 'title' 값 처리
+            string selectedTitle = comboBoxTitles.SelectedItem.ToString();
+            //Console.WriteLine($"선택한 'title': {selectedTitle}");
+
+            // 선택된 'title'에 해당하는 'photo' 열의 바이너리 데이터 가져오기
+            byte[] photoBytes = GetPhotoData(selectedTitle);
+
+            if (photoBytes != null && photoBytes.Length > 0)
+            {
+                // 바이너리 데이터를 이미지로 변환
+                Image image = ByteArrayToImage(photoBytes);
+
+                // 이미지를 PictureBox에 설정하여 디자인 탭에서 표시
+                Pic3.Image = image;
+            }
+            else
+            {
+                Console.WriteLine("선택한 'title'에 대한 이미지가 없습니다.");
+            }
+        }
+
+        //이미지 바이너리 데이터 가져오기
+        private byte[] GetPhotoData(string title)
+        {
+            string connString = "Host=localhost;Port=5432;Username=test;Password=test;Database=TEST2";
+
+            using (var conn = new NpgsqlConnection(connString))
+            {
+                try
+                {
+                    conn.Open();
+
+                    // 'title'에 해당하는 'photo' 열의 바이너리 데이터 조회 쿼리
+                    string selectQuery = "SELECT photo FROM data WHERE title = @title";
+
+                    using (var selectCommand = new NpgsqlCommand(selectQuery, conn))
+                    {
+                        // 파라미터 추가
+                        selectCommand.Parameters.AddWithValue("title", title);
+
+                        // 바이너리 데이터 읽기
+                        object result = selectCommand.ExecuteScalar();
+                        if (result != null)
+                        {
+                            return (byte[])result;
+                        }
+                    }
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.ToString());
+                }
+            }
+
+            return null;
+        }
+
+        //바이너리 데이터를 이미지로 변환
+        private Image ByteArrayToImage(byte[] byteArray)
+        {
+            using (MemoryStream ms = new MemoryStream(byteArray))
+            {
+                return Image.FromStream(ms);
+            }
+        }
+    }
+}
